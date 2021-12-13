8505f1a9663eb92b0c590866718438e6c68888dd
Baotong LU <lubaotong22@gmail.com>
2021-12-13 21:30:19 +0800

    adjust build

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-#gcc -shared -o libworm.so -fPIC worm_wrapper.cpp wh.c kv.c lib.c
-gcc -c -fPIC wh.c -o wh.o
-gcc -c -fPIC kv.c -o kv.o
-gcc -c -fPIC lib.c -o lib.o
-gcc -c -fPIC worm_wrapper.cpp -o libwh.o
-gcc wh.o kv.o lib.o libwh.o -shared -o libwh.so
+g++ -shared -o libworm.so -fPIC worm_wrapper.cpp wh.c kv.c lib.c
